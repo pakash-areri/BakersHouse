f1fe94090ac4bf62586f03bdd2855ef36474296b
pakash-areri <74898841+pakash-areri@users.noreply.github.com>
2021-11-18 20:53:07 +0530

    Update browse.php

--- browse.php
+++ browse.php
@@ -23,6 +23,17 @@ h3{
 }
 </style>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2YSY7BEV11"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2YSY7BEV11');
+</script>
+	
+	
 </head>
 <body>
 
