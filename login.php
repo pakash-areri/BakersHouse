f24ff979b75a31f1558af7e0b5990a610aa73c4c
pakash-areri <74898841+pakash-areri@users.noreply.github.com>
2021-11-18 20:54:39 +0530

    Update login.php

--- login.php
+++ login.php
@@ -70,6 +70,15 @@ html,body {
 padding-left:50px;
 }
 </style>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2YSY7BEV11"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2YSY7BEV11');
+</script>
 
 </head>
 <body>
